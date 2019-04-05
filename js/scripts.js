f36bcd9ce8b3c4bf28723f316968c651a983eca3
Brendan Hellar <bwhellar@Brendans-Air.hsd1.wa.comcast.net>
2019-04-05 14:19:49 -0700

    Give some idea of changing certain numbers on command

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,27 @@ $(document).ready(function() {
   $("#num").keyup(function(event){
     event.preventDefault();
     $("#result").html(count($("#num").val()));
-    document.getElementById('result').firstChild.nodeValue = "BOOP";
   })
+  $("#replaceMe").click(function() {
+      $("body").children().each(function() {
+          $(this).html($(this).html().replace(/1/g,"Beep"));
+          $(this).html($(this).html().replace(/2/g,"Boop"));
+          $(this).html($(this).html().replace(/3/g,"I'm sorry, Dave. I'm afraid I can't do that."));
+          $(this).html($(this).html().replace(/13/g,"I'm sorry, Dave. I'm afraid I can't do that."));
+          $(this).html($(this).html().replace(/21/g,"Boop"));
+          $(this).html($(this).html().replace(/32/g,"I'm sorry, Dave. I'm afraid I can't do that."));
+      });
+    });
 
-  function count(num) {
-    var priorNum = "";
-    var newNum = num;
-    for(var i = 1; i <= num; i++) {
-      priorNum += i;
-
-      if (i < num) {
-        priorNum += ' ';
+    function count(num) {
+      var priorNum = "";
+      var newNum = num;
+      for(var i = 0; i <= num; i++) {
+        priorNum += i;
+        if (i < num) {
+          priorNum += ' ';
+        }
       }
+      return ("" + priorNum);
     }
-    return ("" + priorNum);
-  }
-});
+  });
