9ba595792cf76cd89751ead4927321af73091081
Brendan Hellar <bwhellar@Brendans-Air.hsd1.wa.comcast.net>
2019-04-05 11:14:59 -0700

    create beeps, boops, and the sentence on the entry of 1,2, and 3

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,52 @@ $(document).ready(function() {
 
 
     function count(num) {
-      var newNum = num
-      var numberArray = [1,2,3];
-      var replaced = ["Beep", "Boop", "I'm sorry, Dave. I'm afraid I can't do that"]
-      for(var i = 0; newNum.length; i++){
-         if (newNum == numberArray[0]);{
-          $("#result").append(replaced[0])
-        } if (newNum == numberArray[1]);{
-          $("#result").append(replaced[1])
-        } if (newNum == numberArray[2]);{
-          $("#result").append(replaced[2])
+   // Variables //
+    var roman = ["Beep", "Boop", "I'm sorry, Dave. I'm afraid I can't do that"];
+    var equiv = [1, 2, 3];
+    var romanNum = "";
+    var newRome = num;
+    var i;
+    if (newRome == 1){
+      for (i = 0; i <= equiv.length; i++){
+        while (newRome >= equiv[i]){
+          romanNum += roman[i];
+          newRome -= equiv[i];
+        if (equiv[0] == num)
+        return roman [0];
       }
-      return num;
+      }
+    } else if (newRome == 2) {
+      for (i = 0; i <= equiv.length; i++){
+        while (newRome >= equiv[i]){
+          romanNum += roman[i];
+          newRome -= equiv[i];
+        if (equiv[1] == num)
+        return roman [1];
+      }
+      }
+    } else if (newRome == 3) {
+      for (i = 0; i <= equiv.length; i++){
+        while (newRome >= equiv[i]){
+          romanNum += roman[i];
+          newRome -= equiv[i];
+        if (equiv[2] == num)
+        return roman [2];
+      }
+      }
     }
   }
 });
+    //   var newNum = num
+    //   var numberArray = [1,2,3];
+    //   var replaced = ["Beep", "Boop", "I'm sorry, Dave. I'm afraid I can't do that"]
+    //   for(var i = 0; newNum.length; i++){
+    //      if (newNum == numberArray[0]);{
+    //       $("#result").append(replaced[0])
+    //     } if (newNum == numberArray[1]);{
+    //       $("#result").append(replaced[1])
+    //     } if (newNum == numberArray[2]);{
+    //       $("#result").append(replaced[2])
+    //   }
+    //   return num;
+    // }
