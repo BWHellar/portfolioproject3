67563f765b2c74c024bbb9580a70ec92d090664d
Brendan Hellar <bwhellar@Brendans-Air.hsd1.wa.comcast.net>
2019-04-05 10:28:03 -0700

    create a wonderful infinity loop

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,16 @@ $(document).ready(function() {
     function count(num) {
       var newNum = num
       var numberArray = [1,2,3];
+      var replaced = ["Beep", "Boop", "I'm sorry, Dave. I'm afraid I can't do that"]
       for(var i = 0; newNum.length; i++){
-         if (newNum == numberArray);
-          console.log(newNum);
-          return newNum;
-        }
+         if (newNum == numberArray[0]);{
+          $("#result").append(replaced[0])
+        } if (newNum == numberArray[1]);{
+          $("#result").append(replaced[1])
+        } if (newNum == numberArray[2]);{
+          $("#result").append(replaced[2])
+      }
+      return num;
     }
-  });
+  }
+});
