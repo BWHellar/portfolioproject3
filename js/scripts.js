9c6ec6ffd41da78bdf2e6a2254b1057c69904494
Brendan Hellar <bwhellar@Nancys-iPhone.localdomain>
2019-04-06 13:36:26 -0700

    add a changed logic after the desginated due time. previous methodology was overthought and this will help bring it back down to basic and a functional product

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,30 @@
 $(document).ready(function() {
   $("#num").keyup(function(event){
     event.preventDefault();
-    $("#result").html(count($("#num").val())); alter();
+    $("#result").html(count($("#num").val())); spaceIt();
   })
 //Business Logic //
-    function alter(){
+    //This function is to find all instances of "," and give them a space after//
+    function spaceIt(){
       $("body").children().each(function() {
-        var wordArray = ["Beep", "Boop", "I'm sorry, Dave. I'm afraid I can't do that."]
-          $(this).html($(this).html().replace(/1/g, wordArray[0]));
-          $(this).html($(this).html().replace(/2/g, wordArray[1]));
-          $(this).html($(this).html().replace(/3/g, wordArray[2]));
-          $(this).html($(this).html().replace(/13/g, wordArray[2]));
-          $(this).html($(this).html().replace(/21/g, wordArray[1]));
-          $(this).html($(this).html().replace(/32/g, wordArray[2]));
-      });
-    }
-    // Number Counter //
+      $(this).html($(this).html().replace(/,/g, ", "));
+    });
+  }
+    //This function is to both count and replace the selceted numbers that need to be changed//
     function count(num) {
-      var priorNum = "";
+      var priorNum = [];
       var newNum = num;
-      for(var i = 0; i <= num; i++) {
-        priorNum += i;
-        if (i < num) {
-          priorNum += ' ';
-        }
+      var repoNum = ["1","2","3"]
+      var wordArray = ["Beep!", "Boop!", "I'm sorry, Dave. I'm afraid I can't do that"];
+      for(var i = 0; i <= newNum; i++) {
+        if(i.toString().split('').includes(repoNum[3])) {
+          priorNum.push(wordArray[3]);
+        }else if(i.toString().split('').includes(repoNum[2])) {
+          priorNum.push(wordArray[2]);
+        }else if(i.toString().split('').includes(repoNum[1])) {
+          priorNum.push(wordArray[1]);
+        }else priorNum.push(i);
       }
-      return ("" + priorNum);
+      return (priorNum + ",");
     }
   });
